3ba0f40dfb8d5a42c0c2ee534a4e6060f0bc083f
Anton Shchatsinin <shhetinin.toni@bk.ru>
2019-12-21 16:00:41 +0300

    Technical task

---
+++ index.js
@@ -0,0 +1,17 @@
+// $('.control').click( function(){
+//     $('body').addClass('search-active');
+//     $('.input-search').focus();
+//   });
+  
+//   $('.icon-close').click( function(){
+//     $('body').removeClass('search-active');
+//   });
+
+
+
+function openSearch() {
+    document.getElementById("mylayout").style.display = "block";
+}
+function closeSearch() {
+    document.getElementById("mylayout").style.display = "none";
+}
